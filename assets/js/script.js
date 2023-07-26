59903b66e98d2d01031bc62e05fcdc788881406f
Bishal <bishal.tech.com@gmail.com>
2023-07-26 14:49:59 +0530

    add workspace page

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,7 @@ $(document).ready(function () {
       // instead of a settings object
     ],
   });
+
   $("#gallery_slider2").slick({
     dots: false,
     rtl: true,
@@ -72,6 +73,40 @@ $(document).ready(function () {
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
+    ],
+  });
+
+  $(".pricing-slider").slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: fa,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
     ],
   });
 });
