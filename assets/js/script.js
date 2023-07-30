f6cebd8b0c3c15322b05c46555b924cb09db0361
Bishal <bishal.tech.com@gmail.com>
2023-07-30 20:57:38 +0530

    fix cafe box image

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,4 +153,38 @@ $(document).ready(function () {
       },
     ],
   });
+
+  $(".flip-card-slider").slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: fa,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
